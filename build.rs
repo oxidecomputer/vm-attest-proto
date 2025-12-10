a888709b4b9e3b3f0870739047cc281bbb2b20f8
Philip Tricca <flihp@twobit.org>
2025-12-09 16:41:29 -0800

    attest-time: add tool to time calls to `AttestMock::attest`

--- build.rs
+++ build.rs
@@ -80,7 +80,19 @@ fn write_path_to_conf(mut file: &File, path: &Path, name: &str) -> Result<()> {
     )?)
 }
 
+/// This path is where Oxide specific libraries live on helios systems.
+/// The linker needs this path to find libipcc:
+/// https://github.com/oxidecomputer/ipcc-rs/
+#[cfg(target_os = "illumos")]
+static OXIDE_PLATFORM: &str = "/usr/platform/oxide/lib/amd64/";
+
 fn main() -> Result<()> {
+    #[cfg(target_os = "illumos")]
+    {
+        println!("cargo:rustc-link-arg=-Wl,-R{}", OXIDE_PLATFORM);
+        println!("cargo:rustc-link-search={}", OXIDE_PLATFORM);
+    }
+
     let cwd = env::current_dir().context("get current dir")?;
     let mut cwd = path::absolute(cwd).context("current_dir to absolute")?;
 
